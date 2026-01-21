a732a1e9b907a963fd3817c10fc6cf18ae9da81d
0mptll <omp9979989@gmail.com>
2026-01-21 17:07:00 +0530

    Fix the RGBA value update dynamically to the hero section

--- main.js
+++ main.js
@@ -10,30 +10,51 @@ const bValue = document.getElementById("bValue");
 const aSlider = document.getElementById("aSlider");
 const aValue = document.getElementById("aValue");
 
+const heroText = document.querySelector(".hero-style");
+
 // slider -> number
 rSlider.addEventListener("input",()=>{
     rValue.value = rSlider.value;
+    updateRGBA();
 });
 gSlider.addEventListener("input",()=>{
     gValue.value = gSlider.value;
+    updateRGBA();
 });
 bSlider.addEventListener("input",()=>{
     bValue.value = bSlider.value;
+    updateRGBA();
 });
 aSlider.addEventListener("input",()=>{
     aValue.value = aSlider.value;
+    updateRGBA();
 });
 
 // number -> slider
 rValue.addEventListener("input",()=>{
     rSlider.value = rValue.value;
+    updateRGBA();
 });
 gValue.addEventListener("input",()=>{
     gSlider.value = gValue.value;
+    updateRGBA();
 });
 bValue.addEventListener("input",()=>{
     bSlider.value = bValue.value;
+    updateRGBA();
 });
 aValue.addEventListener("input",()=>{
     aSlider.value = aValue.value;
+    updateRGBA();
 });
+
+function updateRGBA() {
+  const r = rSlider.value;
+  const g = gSlider.value;
+  const b = bSlider.value;
+  const a = aSlider.value;
+
+  heroText.textContent = `rgba(${r}, ${g}, ${b}, ${a})`;
+}
+
+updateRGBA();
