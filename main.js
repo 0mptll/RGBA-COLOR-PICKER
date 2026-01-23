58e6092c84a8772f668700fca0ad267d8f5d3eb0
0mptll <omp9979989@gmail.com>
2026-01-23 14:39:25 +0530

    Add the algorithm for rgb->hsl

--- main.js
+++ main.js
@@ -42,6 +42,48 @@ function rgbToHsvString(r, g, b) {
     return `hsv(${hue}, ${sat}%, ${val}%)`;
 }
 
+
+
+function rgbToHslString(r, g, b) {
+  // convert r,g,b [0,255] range to [0,1]
+    r = r / 255,
+    g = g / 255,
+    b = b / 255;
+    // get the min and max of r,g,b
+    var max = Math.max(r, g, b);
+    var min = Math.min(r, g, b);
+    // lightness is the average of the largest and smallest color components
+    var lum = (max + min) / 2;
+    var hue;
+    var sat;
+    if (max == min) { // no saturation
+        hue = 0;
+        sat = 0;
+    } else {
+        var c = max - min; // chroma
+        // saturation is simply the chroma scaled to fill
+        // the interval [0, 1] for every combination of hue and lightness
+        sat = c / (1 - Math.abs(2 * lum - 1));
+        switch(max) {
+            case r:
+                hue = (g - b) / c;
+                hue = ((g - b) / c) % 6;
+                hue = (g - b) / c + (g < b ? 6 : 0);
+                break;
+            case g:
+                hue = (b - r) / c + 2;
+                break;
+            case b:
+                hue = (r - g) / c + 4;
+                break;
+        }
+    }
+    hue = Math.round(hue * 60); // °
+    sat = Math.round(sat * 100); // %
+    lum = Math.round(lum * 100); // %
+    return `hsl(${hue}, ${sat}%, ${lum}%)`;
+}
+
 function updateMultiColorDisplay() {
     const r = parseInt(rSlider.value);
     const g = parseInt(gSlider.value);
@@ -63,6 +105,9 @@ function updateMultiColorDisplay() {
             break;
         case "hsla": // This maps to your HSV option in the HTML
             displayValue = rgbToHsvString(r, g, b);
+            break;
+        case "hsl":
+            displayValue = rgbToHslString(r, g, b);
             break;
     }
 
